feaaeeb70538971e8b24a56c5f86f4bec469338f
Explorer001 <lukas.gehreke@gmail.com>
2018-12-15 16:51:40 +0100

    added input for basic and matrix

--- src/Base.java
+++ src/Base.java
@@ -3,7 +3,7 @@
 /**
  * TODO description
  */
-abstract class Base$$Basic {
+public class Base {
 
 	public double add(double i, double j) {
 		return i+j;
@@ -18,34 +18,7 @@ abstract class Base$$Basic {
 	}
 	
 	public double div(double i, double j) {
-		return i/j;
-	}
-	
-}
-
-/**
- * TODO description
- */
-public class Base extends  Base$$Basic  {
-	
-	public double add(double i, double j) {
-		System.out.println("Adding " + Double.toString(i) + " and " + Double.toString(j));
-		return super.add(i,j);
-	}
-	
-	public double sub(double i, double j) {
-		System.out.println("Substract " + Double.toString(i) + " and " + Double.toString(j));
-		return super.sub(i,j);
-	}
-	
-	public double mul(double i, double j) {
-		System.out.println("Mult " + Double.toString(i) + " and " + Double.toString(j));
-		return super.mul(i,j);
-	}
-	
-	public double div(double i, double j) {
-		System.out.println("Div " + Double.toString(i) + " and " + Double.toString(j));
-		return super.div(i,j);
+		return (j == 0)? 0 : i/j;
 	}
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -8,33 +8,107 @@ import java.util.*;
  */
 public class Main {
 	
-	public static void main(String args[]) {
-		/*Scanner sc = new Scanner(System.in);
+	static Logging l = new Logging("log");
+	static Scanner sc = new Scanner(System.in);
+	
+	public static boolean eval() {
 		String rd = "";
-		while (rd != "q\n") {
-			System.out.println("Enter expression: ");
-			rd = sc.nextLine();
-			System.out.println(rd);
+		boolean rv = true;
+		System.out.println("1: Basic math");
+		System.out.println("2: Matrix");
+		System.out.println("3: Round");
+		System.out.println("4: Logic");
+		System.out.println("5: Trigonometric");
+		rd = sc.nextLine();
+		System.out.println(rd);
+		if (rd.equals("q")) {
+			rv = false;
+		} else if (rd.equals("1")) {
+			eval_base();
+		} else if (rd.equals("2")) {
+			eval_mat();
+		} else if (rd.equals("3")) {
+			eval_round();
+		} else if (rd.equals("4")) {
+			eval_logic();
+		} else if (rd.equals("5")) {
+			eval_trig();
+		}
+		return rv;
+	}
+	
+	public static void eval_base() {
+		double v1, v2;
+		String rd = "";
+		System.out.print("Val 1: ");
+		v1 = Double.parseDouble(sc.nextLine());
+		System.out.println();
+		System.out.print("Val 2: ");
+		v2 = Double.parseDouble(sc.nextLine());
+		System.out.println();
+		System.out.println("1: Add");
+		System.out.println("2: Sub");
+		System.out.println("3: Mult");
+		System.out.println("4: Div");
+		rd = sc.nextLine();
+		System.out.println(rd);
+		if (rd.equals("1")) {
+			double s = new Base().add(v1, v2);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log(Double.toString(v1) + " + " + Double.toString(v2) + " = " + Double.toString(s));
+		} else if (rd.equals("2")) {
+			double s = new Base().sub(v1, v2);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log(Double.toString(v1) + " - " + Double.toString(v2) + " = " + Double.toString(s));
+		} else if (rd.equals("3")) {
+			double s = new Base().mul(v1, v2);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log(Double.toString(v1) + " * " + Double.toString(v2) + " = " + Double.toString(s));
+		} else if (rd.equals("4")) {
+			double s = new Base().div(v1, v2);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log(Double.toString(v1) + " / " + Double.toString(v2) + " = " + Double.toString(s));
+		}
+	}
+	
+	public static Matrix eval_mat() {
+		int n,m;
+		System.out.println("Matrix size:");
+		System.out.print("n=");
+		n = Integer.parseInt(sc.nextLine());
+		System.out.println();
+		System.out.print("m=");
+		m = Integer.parseInt(sc.nextLine());
+		System.out.println();
+		Matrix M = new Matrix(n, m);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print("("+Integer.toString(i)+", "+Integer.toString(j)+")=");
+				M.set(i,j,Double.parseDouble(sc.nextLine()));
+			}
+		}
+		M.print();
+		return M;
+	}
+	
+	public static void eval_round() {
+		
+	}
+	
+	public static void eval_logic() {
+		
+	}
+	
+	public static void eval_trig() {
+		
+	}
+	
+	public static void main(String args[]) {
+		boolean run = true;
+		while (run) {
+			run = eval();
 		}
 		sc.close();
-		*/
-		System.out.println(new Base().add((float) 1.0, (float) 1.0));
-		System.out.println(new Base().add(1, 1));
-		System.out.println(new Root().root(4,2));
-		Matrix a = new Matrix(2,2);
-		a.set(0,0,1);
-		a.set(0,1,1);
-		a.set(1,0,1);
-		a.set(1,1,1);
-		Matrix b = new Matrix(2,2);
-		b.set(0,0,1);
-		b.set(0,1,1);
-		b.set(1,0,1);
-		b.set(1,1,1);
-		Matrix c = new MatMult().mult(a, b);
-		c.print();
-		Matrix d = new ScalarMult().mult(2, c);
-		d.print();
 	}
 	
 }
