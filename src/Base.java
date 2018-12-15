c84f0c4ed720910808c7d726463e0a642a4a39b0
Explorer001 <lukas.gehreke@gmail.com>
2018-12-15 17:21:08 +0100

    added all basic parsers

--- src/Base.java
+++ src/Base.java
@@ -3,7 +3,7 @@
 /**
  * TODO description
  */
-public class Base {
+abstract class Base$$Basic {
 
 	public double add(double i, double j) {
 		return i+j;
@@ -19,6 +19,33 @@ public class Base {
 	
 	public double div(double i, double j) {
 		return (j == 0)? 0 : i/j;
+	}
+	
+}
+
+/**
+ * TODO description
+ */
+public class Base extends  Base$$Basic  {
+	
+	public double add(double i, double j) {
+		System.out.println("Adding " + Double.toString(i) + " and " + Double.toString(j));
+		return super.add(i,j);
+	}
+	
+	public double sub(double i, double j) {
+		System.out.println("Substract " + Double.toString(i) + " and " + Double.toString(j));
+		return super.sub(i,j);
+	}
+	
+	public double mul(double i, double j) {
+		System.out.println("Mult " + Double.toString(i) + " and " + Double.toString(j));
+		return super.mul(i,j);
+	}
+	
+	public double div(double i, double j) {
+		System.out.println("Div " + Double.toString(i) + " and " + Double.toString(j));
+		return super.div(i,j);
 	}
 	
 }
--- src/Main.java
+++ src/Main.java
@@ -92,15 +92,136 @@ public class Main {
 	}
 	
 	public static void eval_round() {
-		
+		double v1;
+		String rd = "";
+		System.out.print("Val 1: ");
+		v1 = Double.parseDouble(sc.nextLine());
+		System.out.println();
+		System.out.println();
+		System.out.println("1: Floor");
+		System.out.println("2: Ceil");
+		rd = sc.nextLine();
+		System.out.println(rd);
+		if (rd.equals("1")) {
+			double s = new Round().floor(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("floor("+Double.toString(v1)+") = " + Double.toString(s));
+		} else if (rd.equals("2")) {
+			double s = new Round().ceil(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("ceil("+Double.toString(v1)+") = " + Double.toString(s));
+		}
 	}
 	
 	public static void eval_logic() {
-		
+		int v1, v2;
+		System.out.print("Val 1: ");
+		v1 = sc.nextInt();
+		System.out.println();
+		System.out.print("Val 2: ");
+		v2 = sc.nextInt();
+		System.out.println();
+		System.out.println("1: and");
+		System.out.println("2: or");
+		System.out.println("3: not");
+		System.out.println("4: xor");
+		System.out.println("5: nor");
+		System.out.println("6: nand");
+		System.out.println("7: directional");
+		System.out.println("8: bidirectional");
+		System.out.print("Selection=");
+		int sel = sc.nextInt();
+		int s;
+		switch (sel) {
+		case 1:
+			s = new Logic().and(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " and "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		case 2:
+			s = new Logic().or(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " or "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		case 3:
+			s = new Logic().not(v1);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log("not " + Integer.toString(v1) +" = "+Integer.toString(s));
+			break;
+		case 4:
+			s = new Logic().xor(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " xor "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		case 5:
+			s = new Logic().nor(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " nor "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		case 6:
+			s = new Logic().nand(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " nand "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		case 7:
+			s = new Logic().directional(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " => "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		case 8:
+			s = new Logic().bidirectional(v1, v2);
+			System.out.println("Solution is " + Integer.toString(s));
+			l.log(Integer.toString(v1)+ " <=> "+Integer.toString(v2)+" = "+Integer.toString(s));
+			break;
+		}
 	}
 	
 	public static void eval_trig() {
-		
+		double v1;
+		System.out.print("Val 1: ");
+		v1 = sc.nextDouble();
+		System.out.println();
+		System.out.println("1: sin");
+		System.out.println("2: cos");
+		System.out.println("3: tan");
+		System.out.println("4: asin");
+		System.out.println("5: acos");
+		System.out.println("6: atan");
+		System.out.print("Selection=");
+		int sel = sc.nextInt();
+		double s;
+		switch (sel) {
+		case 1:
+			s = new Trigonometric().sin(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("sin("+Double.toString(v1)+") = "+Double.toString(s));
+			break;
+		case 2:
+			s = new Trigonometric().cos(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("cos("+Double.toString(v1)+") = "+Double.toString(s));
+			break;
+		case 3:
+			s = new Trigonometric().tan(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("tan("+Double.toString(v1)+") = "+Double.toString(s));
+			break;
+		case 4:
+			s = new Trigonometric().asin(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("asin("+Double.toString(v1)+") = "+Double.toString(s));
+			break;
+		case 5:
+			s = new Trigonometric().acos(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("acos("+Double.toString(v1)+") = "+Double.toString(s));
+			break;
+		case 6:
+			s = new Trigonometric().atan(v1);
+			System.out.println("Solution is " + Double.toString(s));
+			l.log("atan("+Double.toString(v1)+") = "+Double.toString(s));
+			break;
+		}
 	}
 	
 	public static void main(String args[]) {
