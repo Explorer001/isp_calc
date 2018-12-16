f64dd9617a18d6cdf17e86d9f55c81fd992372b5
Explorer001 <lukas.gehreke@gmail.com>
2018-12-16 19:32:00 +0100

    added matrix parsers

--- src/Base.java
+++ src/Base.java
@@ -26,7 +26,7 @@ abstract class Base$$Basic {
 /**
  * TODO description
  */
-abstract class Base$$C extends  Base$$Basic  {
+public class Base extends  Base$$Basic  {
 
 	public Complex add(Complex a, Complex b) {
 		return new Complex(a.real+b.real, a.imag+b.imag);
@@ -36,33 +36,6 @@ abstract class Base$$C extends  Base$$Basic  {
 		double new_real = (a.real*b.real)-(a.imag*b.imag);
 		double new_imag = (a.real*b.imag)+(a.imag*b.real);
 		return new Complex(new_real, new_imag);
-	}
-	
-}
-
-/**
- * TODO description
- */
-public class Base extends  Base$$C  {
-	
-	public double add(double i, double j) {
-		System.out.println("Adding " + Double.toString(i) + " and " + Double.toString(j));
-		return super.add(i,j);
-	}
-	
-	public double sub(double i, double j) {
-		System.out.println("Substract " + Double.toString(i) + " and " + Double.toString(j));
-		return super.sub(i,j);
-	}
-	
-	public double mul(double i, double j) {
-		System.out.println("Mult " + Double.toString(i) + " and " + Double.toString(j));
-		return super.mul(i,j);
-	}
-	
-	public double div(double i, double j) {
-		System.out.println("Div " + Double.toString(i) + " and " + Double.toString(j));
-		return super.div(i,j);
 	}
 	
 }
